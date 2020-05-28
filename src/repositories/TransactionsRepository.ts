ca88d54983adca05f5ae1794ca5de76b9b1ab264
DanielSilvado <danielsilvado@id.uff.br>
2020-05-27 21:30:45 -0300

    implementing method POST

---
+++ src/repositories/TransactionsRepository.ts
@@ -0,0 +1,44 @@
+import Transaction from '../models/Transaction';
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+interface CreateTransation {
+  title: string
+  value: number,
+  type: 'income' | 'outcome'
+}
+
+
+class TransactionsRepository {
+  private transactions: Transaction[];
+
+  constructor() {
+    this.transactions = [];
+  }
+
+  public all(): Transaction[] {
+    // TODO
+  }
+
+  public getBalance(): Balance {
+    // TODO
+  }
+
+  public create({ title, value, type }: CreateTransation): Transaction {
+    const transaction = new Transaction({
+      title,
+      value,
+      type
+    })
+
+    this.transactions.push(transaction)
+
+    return transaction
+  }
+}
+
+export default TransactionsRepository;
